766b5ca15729fabd04f80a38bb5072793cd27ce3
Damian Olzheim <d.olzheim@alphacore.nl>
2021-05-30 21:26:23 +0200

    Added test for home controller

---
+++ CasusBlok4.Test/HomeControllerTest.cs
@@ -0,0 +1,61 @@
+﻿using CasusBlok4.Controllers;
+using CasusBlok4.Views.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CasusBlok4.Test
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index()
+        {
+            // act
+            HomeController controller = new HomeController();
+
+            // arrange
+            IActionResult result = controller.Index();
+
+            // assert
+            Assert.IsAssignableFrom<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Privacy()
+        {
+            // act
+            HomeController controller = new HomeController();
+
+            // arrange
+            IActionResult result = controller.Privacy();
+
+            // assert
+            Assert.IsAssignableFrom<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Error()
+        {
+            // act
+            HomeController controller = new HomeController
+            {
+                ControllerContext = new ControllerContext()
+            };
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            // arrange
+            IActionResult result = controller.Error();
+
+            // assert
+            Assert.IsAssignableFrom<ViewResult>(result);
+            ViewResult view = (ViewResult) result;
+            Assert.IsAssignableFrom<ErrorViewModel>(view.Model);
+        }
+    }
+}
