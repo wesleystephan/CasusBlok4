c2f8e325bf074c8afe3a6f05d29e1204265f6b69
Damian Olzheim <d.olzheim@alphacore.nl>
2021-06-03 15:12:45 +0200

    Added my transactions

--- CasusBlok4/Controllers/HomeController.cs
+++ CasusBlok4/Controllers/HomeController.cs
@@ -1,18 +1,24 @@
 ﻿using CasusBlok4.Models;
+using CasusBlok4.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace CasusBlok4.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
+        private readonly DataContext _dataContext;
+
+        public HomeController(DataContext dataContext)
         {
+            _dataContext = dataContext;
         }
 
         public IActionResult Index()
@@ -23,6 +29,13 @@ namespace CasusBlok4.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [Authorize]
+        public IActionResult MyTransactions()
+        {
+            int customerId = int.Parse(User.FindFirst(q => q.Type == ClaimTypes.NameIdentifier).Value);
+            return View(_dataContext.Transactions.Where(q=>q.CustomerId == customerId));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
